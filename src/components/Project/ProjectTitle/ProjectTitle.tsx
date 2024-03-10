ac46f5336118b97a1b04be3fa45edebebb1f376f
Leyson <134742017+leysonn@users.noreply.github.com>
2024-03-10 16:07:55 +0300

    Начата работа над страницей проекта, написан компонент ProjectTitle

---
+++ src/components/Project/ProjectTitle/ProjectTitle.tsx
@@ -0,0 +1,18 @@
+import { ProjectItem } from '../../../constants/projectsList';
+import styles from './ProjectTitle.module.scss';
+
+type ProjectTitleProps = {
+    project: ProjectItem;
+};
+
+function ProjectTitle({ project }: ProjectTitleProps) {
+    return (
+        <div className={styles.titleContainer}>
+            <h1 className={styles.title}>{project.title}</h1>
+            <p className={styles.subtitle}>Agency provides a full service range including technical skills, design, business understanding.</p>
+            <p className={styles.description}>{project.description}</p>
+        </div>
+    );
+}
+
+export default ProjectTitle;
--- src/pages/Project.tsx
+++ src/pages/Project.tsx
@@ -1,10 +1,33 @@
-// import styles from './Project.module.scss';
 import { Route } from '../routes/portfolio/$projectId';
+import { ProjectItem, projectsList } from '../constants/projectsList';
+import Title from '../components/Title/Title';
+import ProjectTitle from '../components/Project/ProjectTitle/ProjectTitle';
 
 function Project() {
     const { projectId } = Route.useParams();
+    const project: ProjectItem | undefined = projectsList.projects.find(project => project.id === projectId);
 
-    return <h1>Project #{projectId}</h1>;
+    if (!project)
+        return (
+            <Title description={`Project #${projectId} Not Found`} className="mb-20">
+                4 0 4
+            </Title>
+        );
+
+    return (
+        <main>
+            {/* <Back to="/portfolio" url="/portfolio" /> */}
+            <ProjectTitle project={project} />
+            {/* <ProjectInfo /> */}
+            {/* <img /> */}
+            {/* <ProjectSection title="Problem" description={} /> */}
+            {/* <Images /> */}
+            {/* <ProjectSection title="Solution" description={} /> */}
+            {/* <Share /> */}
+            {/* <Hr /> */}
+            {/* <RelatedProjects /> */}
+        </main>
+    );
 }
 
 export default Project;
