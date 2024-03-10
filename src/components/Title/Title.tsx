d9c1c9d11c2801e6c3fec6bcf980cd2de80c2c7e
Leyson <134742017+leysonn@users.noreply.github.com>
2024-03-10 16:07:17 +0300

    Добавлена возможность изменения описания и добавления классов для компонента Title

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -3,14 +3,18 @@ import styles from './Title.module.scss';
 type TitleProps = {
     children: string;
     higher?: boolean;
+    description?: string;
+    className?: string;
 };
 
-function Title({ children, higher = false }: TitleProps) {
+function Title({ children, higher = false, description, className }: TitleProps) {
     return (
-        <section id="title">
+        <section id="title" className={className}>
             <div className={higher ? styles.titleHigher : styles.titleContent}>
                 <h1 className={styles.title}>{children}</h1>
-                <p className={styles.description}>Agency provides a full service range including technical skills, design, business understanding.</p>
+                <p className={styles.description}>
+                    {description ? description : 'Agency provides a full service range including technical skills, design, business understanding.'}
+                </p>
             </div>
         </section>
     );
