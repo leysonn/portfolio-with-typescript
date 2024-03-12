265134b91eb8541119f9a2215d4c509dd733e364
Leyson <134742017+leysonn@users.noreply.github.com>
2024-03-12 19:02:52 +0300

    Исправлены иконки

--- src/components/Home/ServiceSection/ServiceCard/ServiceCard.tsx
+++ src/components/Home/ServiceSection/ServiceCard/ServiceCard.tsx
@@ -6,7 +6,9 @@ import { Link } from '@tanstack/react-router';
 function ServiceCard({ icon, title, description, button, url }: ServicesListItem) {
     return (
         <div className={styles.serviceCard}>
-            <img className={styles.icon} src={icon} alt="Service icon" draggable={false} />
+            <div className={styles.iconContainer}>
+                <img className={styles.icon} src={icon} alt="Service icon" draggable={false} />
+            </div>
             <h4 className={styles.title}>{title}</h4>
             <p className={styles.description}>{description}</p>
             <Link to={url}>
