91590752e808f7e90b2c32f1cc675cbc8b27d251
Leyson <134742017+leysonn@users.noreply.github.com>
2024-03-12 18:50:53 +0300

    Переименованны папки с секциями

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,10 @@
-import HeroSection from '../components/HomeSections/HeroSection/HeroSection';
-import AboutSection from '../components/HomeSections/AboutSection/AboutSection';
-import PartnersSection from '../components/HomeSections/PartnersSection/PartnersSection';
-import HowWeWorkSection from '../components/HomeSections/HowWeWorkSection/HowWeWorkSection';
-import VideoReelSection from '../components/HomeSections/VideoReelSection/VideoReelSection';
-import FeaturesSection from '../components/HomeSections/FeaturesSection/FeaturesSection';
-import ServiceSection from '../components/HomeSections/ServiceSection/ServiceSection';
+import HeroSection from '../components/Home/HeroSection/HeroSection';
+import AboutSection from '../components/Home/AboutSection/AboutSection';
+import PartnersSection from '../components/Home/PartnersSection/PartnersSection';
+import HowWeWorkSection from '../components/Home/HowWeWorkSection/HowWeWorkSection';
+import VideoReelSection from '../components/Home/VideoReelSection/VideoReelSection';
+import FeaturesSection from '../components/Home/FeaturesSection/FeaturesSection';
+import ServiceSection from '../components/Home/ServiceSection/ServiceSection';
 
 function Home() {
     return (
--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -1,5 +1,5 @@
 import Hr from '../components/Hr/Hr';
-import ServicesSection from '../components/ServicesSections/ServicesSection/ServicesSection';
+import ServicesSection from '../components/Services/ServicesSection/ServicesSection';
 import Title from '../components/Title/Title';
 
 function Services() {
