da027c2677bedddba02c6e7f8a3d172900824684
Leyson <134742017+leysonn@users.noreply.github.com>
2024-03-15 12:13:52 +0300

    Написан компонент FeaturesSection

---
+++ src/components/AboutUs/FeaturesSection/FeaturesSection.tsx
@@ -0,0 +1,41 @@
+import styles from './FeaturesSection.module.scss';
+import { statList } from '../../../constants/statList';
+import card_icon from '../../../assets/icons/card_icon.svg';
+import Section from '../../Section/Section';
+import StatCards from '../../StatCards/StatCards';
+
+function FeaturesSection() {
+    return (
+        <section id="features">
+            <Section
+                className={styles.featuresCards}
+                titleContainerStyle={styles.titleContainer}
+                name="Features"
+                title={{ content: "Long run, and work as an extension of the merchant's team.", style: styles.title }}
+                description={{
+                    content: "Ability to put themselves in the merchant's shoes. It is meant to partner on the long run, and work as an extension.",
+                    style: styles.description,
+                }}
+                aside={{
+                    element: (
+                        <div className={styles.cardContainer}>
+                            <div className={styles.cardWhite}>
+                                <img src={card_icon} alt="Card icon" />
+                                <h3 className={styles.cardTitle}>Professional</h3>
+                                <p className={styles.cardDescription}>Full service range including technical skills, design.</p>
+                            </div>
+                            <div className={styles.card}>
+                                <h3 className={styles.cardTitle}>Accessibility</h3>
+                                <p className={styles.cardDescription}>Business understanding, ability to put themselves.</p>
+                            </div>
+                        </div>
+                    ),
+                    position: 'after',
+                }}
+            />
+            <StatCards className={styles.statCards} stats={statList} />
+        </section>
+    );
+}
+
+export default FeaturesSection;
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,4 +1,5 @@
 import DescriptionSection from '../components/AboutUs/DescriptionSection/DescriptionSection';
+import FeaturesSection from '../components/AboutUs/FeaturesSection/FeaturesSection';
 import Gallery from '../components/Gallery/Gallery';
 import Title from '../components/Title/Title';
 import { aboutGallery } from '../constants/aboutGallery';
@@ -9,6 +10,7 @@ function About() {
             <Title>About Us</Title>
             <DescriptionSection />
             <Gallery video={aboutGallery.video} images={aboutGallery.images} />
+            <FeaturesSection />
         </main>
     );
 }
