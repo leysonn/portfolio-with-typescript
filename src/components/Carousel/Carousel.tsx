bd5e1d20945746654b4137bc89df57509df9062c
Leyson <134742017+leysonn@users.noreply.github.com>
2024-03-18 15:17:57 +0300

    Написан компонент Carousel

---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,88 @@
+import { useCallback, useState } from 'react';
+import { useSwipeable } from 'react-swipeable';
+import styles from './Carousel.module.scss';
+import left_arrow from '../../assets/icons/left_arrow_icon.svg';
+import right_arrow from '../../assets/icons/right_arrow_icon.svg';
+
+type List = Array<{ [key: string]: string }>;
+
+type CarouselProps = {
+    list: List;
+    buttons?: boolean;
+    titleContainer: {
+        name: string;
+        title: string;
+        className?: string;
+    };
+    carousel: {
+        content: (item: List[number], index: number) => JSX.Element;
+        itemWidth: number;
+        className: string;
+        containerClassName: string;
+    };
+};
+
+function Carousel({ list, buttons = false, titleContainer, carousel }: CarouselProps) {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    // swipe handlers
+    function handleSwipe(direction: 'left' | 'right'): void {
+        if (direction === 'left') {
+            setActiveIndex(prevIndex => (prevIndex === list.length - 1 ? prevIndex : prevIndex + 1));
+        } else if (direction === 'right') {
+            setActiveIndex(prevIndex => (prevIndex === 0 ? prevIndex : prevIndex - 1));
+        }
+    }
+
+    const swipeHandlers = useSwipeable({
+        onSwipedLeft: () => handleSwipe('left'),
+        onSwipedRight: () => handleSwipe('right'),
+        preventScrollOnSwipe: true,
+        trackMouse: true,
+    });
+
+    // button handlers
+    const handleButtonLeft = useCallback((): void => {
+        setActiveIndex(prevIndex => (prevIndex === 0 ? prevIndex : prevIndex - 1));
+    }, []);
+
+    const handleButtonRight = useCallback((): void => {
+        setActiveIndex(prevIndex => (prevIndex === list.length - 1 ? prevIndex : prevIndex + 1));
+    }, [list]);
+
+    return (
+        <section className={styles.teamCarousel}>
+            <div className={styles.container}>
+                <div className={styles.titleContainer + (titleContainer.className ? ' ' + titleContainer.className : '')}>
+                    <span className={styles.name}>{titleContainer.name}</span>
+                    <h2 className={styles.title}>{titleContainer.title}</h2>
+                </div>
+                {buttons && (
+                    <div className={styles.buttonsContainer}>
+                        <button className={styles.button} onClick={handleButtonLeft}>
+                            <img className={styles.icon} src={left_arrow} alt="Left arrow icon" draggable={false} />
+                        </button>
+                        <button className={styles.button} onClick={handleButtonRight}>
+                            <img className={styles.icon} src={right_arrow} alt="Right arrow icon" draggable={false} />
+                        </button>
+                    </div>
+                )}
+            </div>
+            <div className={`${styles.carousel} ${carousel.className}`} {...swipeHandlers}>
+                <div
+                    className={`${styles.carouselContainer} ${carousel.containerClassName}`}
+                    style={{ transform: `translateX(-${activeIndex * carousel.itemWidth}rem)` }}
+                >
+                    {list.map(carousel.content)}
+                </div>
+            </div>
+            <div className={styles.dotsContainer}>
+                {list.map((_, index) => (
+                    <div key={index} className={index === activeIndex ? styles.dotActive : styles.dot} onClick={() => setActiveIndex(index)} />
+                ))}
+            </div>
+        </section>
+    );
+}
+
+export default Carousel;
