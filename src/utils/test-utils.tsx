83fc5669472840b601bdf5a9d93146bb02ab8712
Leyson <134742017+leysonn@users.noreply.github.com>
2024-03-21 15:31:06 +0300

    Установлены тесты

---
+++ src/utils/test-utils.tsx
@@ -0,0 +1,34 @@
+import { Outlet, RouterProvider, createRootRoute, createRoute, createRouter } from '@tanstack/react-router';
+import { cleanup, render } from '@testing-library/react';
+import { afterEach } from 'vitest';
+
+afterEach(() => {
+    cleanup();
+});
+
+function createTestRouter(component: () => React.ReactElement) {
+    const rootRoute = createRootRoute({
+        component: Outlet,
+    });
+
+    const componentRoute = createRoute({
+        getParentRoute: () => rootRoute,
+        path: '/',
+        component,
+    });
+
+    const router = createRouter({
+        routeTree: rootRoute.addChildren([componentRoute]),
+    });
+
+    return router;
+}
+
+function customRender(ui: () => React.ReactElement, options = {}) {
+    const router = createTestRouter(ui);
+    return render(<RouterProvider router={router} />, options);
+}
+
+// eslint-disable-next-line react-refresh/only-export-components
+export * from '@testing-library/react';
+export { customRender as render };
--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,15 @@
-import { defineConfig } from 'vite';
+import { ConfigEnv, UserConfig, defineConfig } from 'vite';
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin';
 import react from '@vitejs/plugin-react-swc';
+import { InlineConfig } from 'vitest';
 
-type ViteConfigInput = {
-    mode: string;
-    command: string;
-};
+type ViteConfig = UserConfig & { test: InlineConfig };
 
 // https://vitejs.dev/config/
-export default (args: ViteConfigInput) => {
+export default (args: ConfigEnv) => {
     const generateScopedName = args.mode === 'development' ? '[local]_[hash:base64:2]' : '[hash:base64:5]';
 
-    return defineConfig({
+    const config: ViteConfig = {
         base: '/portfolio-with-typescript',
         plugins: [react(), TanStackRouterVite()],
         css: {
@@ -20,5 +18,12 @@ export default (args: ViteConfigInput) => {
                 generateScopedName: generateScopedName,
             },
         },
-    });
+        test: {
+            globals: true,
+            environment: 'jsdom',
+            setupFiles: './src/test/setup.ts',
+        },
+    };
+
+    return defineConfig(config);
 };
