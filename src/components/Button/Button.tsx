54225861a64eeeaeb2e3d0abff78425b9c609086
Leyson <134742017+leysonn@users.noreply.github.com>
2024-03-26 16:03:41 +0300

    Улучшена типизация

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,15 @@
+import { ButtonHTMLAttributes } from 'react';
 import styles from './Button.module.scss';
 
-type ButtonProps = {
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children: string | JSX.Element | JSX.Element[];
-    className?: string;
     width: number;
     color: 'light' | 'dark' | 'light_accent' | 'dark_accent' | 'light_transparent' | 'dark_transparent';
-    onClick?(): void;
-};
+}
 
-function Button({ children, className, width, color, onClick }: ButtonProps) {
+function Button({ children, className, width, color, ...props }: ButtonProps) {
     return (
-        <button style={{ width: width + 'rem' }} className={styles[color] + (className ? ' ' + className : '')} onClick={onClick}>
+        <button style={{ width: width + 'rem' }} className={styles[color] + (className ? ' ' + className : '')} {...props}>
             {children}
         </button>
     );
